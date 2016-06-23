d80540374cbf353b6aaa7d325260569fab7db240
Omar Shaikh <omarasifshaikh@hotmail.com>
2016-06-22 17:36:50 -0700

    Initial

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <osmium/io/any_input.hpp>
+
+using namespace std;
+
+int main() {
+    cout<<"Hello world!";
+    //osmium::io::Reader reader("/Users/omar/Downloads/IN");
+    return 0;
+}
